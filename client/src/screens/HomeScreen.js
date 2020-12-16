b2c53686fb4c313afac1cdc9e3729692a6cabc45
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-12-17 00:51:28 +0600

    product detail done

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,28 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import Axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProducts } from '../actions/productactions'
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await Axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
-    <>
-      <h1 className='pt-3'>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </>
+    <div className='pt-3'>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </div>
   )
 }
 
