1887616909bde56898bab3a1834f5696411ec80a
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-12-21 01:37:47 +0600

    10/2 done

--- client/src/store.js
+++ client/src/store.js
@@ -7,6 +7,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducres/productReducers'
 import { cartReducer } from './reducres/cartReduceres'
 import {
@@ -33,6 +34,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
