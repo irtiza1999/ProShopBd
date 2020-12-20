89656d05202e5a193660c8cf3066e872369fe5c5
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-12-20 15:47:52 +0600

    10 done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,11 +4,12 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controlers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router
   .route('/profile')
